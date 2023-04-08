669e0ad163a9eb16cc40eac33f984e8ab22220af
Nishat <nishatbd17@gmail.com>
2023-04-08 14:16:49 +0600

    update

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -19,48 +19,4 @@ export default class UsersController {
 
 
 
-    //  public async index({view}){
-    //    const upUser= await Post.query()
-    //    .preload("reacts")
-    //    .preload("comments",(replyQ)=>{
-    //     replyQ.preload("replies")
-    //     }).orderBy('posts.id', 'desc')
-    //    return upUser
-    // }
-
-    // public async store({response,request}){
-    //     const payload= await request.all()
-    //     await Post.create({
-    //         user_id:payload.user_id,
-    //         post_desc:payload.post_desc,
-    //         disabled:payload.disabled
-    //     })
-    // }
-    // public async update({request,response,params}){
-    //     const payload = await request.all()
-    //     await Post.query().where('id',params.id).update({
-    //         post_desc:payload.post_desc
-    //     })
-    // }
-    // public async delete({request,params}){
-    //     const post = await Post.findOrFail(params.id)
-    //     await post.delete()
-    // }
-
-
-
-
-    // public async store({response,request}){
-    //     const payload= await request.all()
-    //     await React.create({
-    //         post_id:payload.post_id,
-    //         total_react:payload.total_react,
-    //     })
-    // }
-    // public async update({response,request,params}){
-    //     const payload = await request.all()
-    //     await React.query().where('post_id',params.id).update({
-    //         total_react:payload.total_react
-    //     })
-    // }
 }
